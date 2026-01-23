3aa37707515f56848862def8586c30800a3b3988
ctommy <nordik.tommy@gmail.com>
2026-01-23 13:45:44 -0500

    test: add lxmf e2e loopback

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,11 +1,13 @@
 use std::collections::HashSet;
 
+use crate::error::LxmfError;
 use crate::message::WireMessage;
 use crate::reticulum::Adapter;
 
 #[derive(Default)]
 pub struct Router {
     outbound: Vec<WireMessage>,
+    inbound: Vec<WireMessage>,
     delivered: HashSet<[u8; 32]>,
 }
 
@@ -28,5 +30,15 @@ impl Router {
 
     pub fn is_delivered(&self, message_id: &[u8; 32]) -> bool {
         self.delivered.contains(message_id)
+    }
+
+    pub fn send_for_test(&mut self, msg: WireMessage) -> Result<(), LxmfError> {
+        self.outbound.push(msg.clone());
+        self.inbound.push(msg);
+        Ok(())
+    }
+
+    pub fn recv_for_test(&mut self) -> Option<WireMessage> {
+        self.inbound.pop()
     }
 }
