90a95ea19c84b176de55aa12986fa26234fbf0f4
ctommy <nordik.tommy@gmail.com>
2026-01-23 13:42:29 -0500

    feat: track delivery receipts in router

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,9 +1,12 @@
+use std::collections::HashSet;
+
 use crate::message::WireMessage;
 use crate::reticulum::Adapter;
 
 #[derive(Default)]
 pub struct Router {
     outbound: Vec<WireMessage>,
+    delivered: HashSet<[u8; 32]>,
 }
 
 impl Router {
@@ -17,5 +20,13 @@ impl Router {
 
     pub fn outbound_len(&self) -> usize {
         self.outbound.len()
+    }
+
+    pub fn handle_receipt_for_test(&mut self, message_id: [u8; 32]) {
+        self.delivered.insert(message_id);
+    }
+
+    pub fn is_delivered(&self, message_id: &[u8; 32]) -> bool {
+        self.delivered.contains(message_id)
     }
 }
