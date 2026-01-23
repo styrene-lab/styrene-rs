c1da6a7d2db5823d57f8bf58b6a0fce8f18cb751
ctommy <nordik.tommy@gmail.com>
2026-01-23 12:14:08 -0500

    feat: add propagation node storage

--- src/propagation/mod.rs
+++ src/propagation/mod.rs
@@ -1,1 +1,21 @@
-// Placeholder module for propagation node APIs.
+use crate::error::LxmfError;
+use crate::message::WireMessage;
+use crate::storage::Store;
+
+pub struct PropagationNode {
+    store: Box<dyn Store + Send + Sync>,
+}
+
+impl PropagationNode {
+    pub fn new(store: Box<dyn Store + Send + Sync>) -> Self {
+        Self { store }
+    }
+
+    pub fn store(&mut self, msg: WireMessage) -> Result<(), LxmfError> {
+        self.store.save(&msg)
+    }
+
+    pub fn fetch(&self, id: &[u8; 32]) -> Result<WireMessage, LxmfError> {
+        self.store.get(id)
+    }
+}
