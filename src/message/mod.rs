42ed87b28453a8988d243e26551420295518bc9f
ctommy <nordik.tommy@gmail.com>
2026-01-26 10:11:28 -0500

    feat: LXMessage state model

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,8 +1,34 @@
 mod payload;
+mod state;
 mod wire;
 
 pub use payload::Payload;
+pub use state::State;
 pub use wire::WireMessage;
 
-#[derive(Default, Debug, Clone)]
-pub struct Message;
+#[derive(Debug, Clone)]
+pub struct Message {
+    state: State,
+}
+
+impl Message {
+    pub fn new() -> Self {
+        Self {
+            state: State::Generating,
+        }
+    }
+
+    pub fn set_state(&mut self, state: State) {
+        self.state = state;
+    }
+
+    pub fn is_outbound(&self) -> bool {
+        self.state == State::Outbound
+    }
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Self::new()
+    }
+}
